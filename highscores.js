268c95b8cb5fc87bebff1679d7ab0ca34918a839
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-01-06 23:46:26 -0600

    code cleanup and reorg

--- highscores.js
+++ highscores.js
@@ -4,19 +4,22 @@ var initialsForm = document.querySelector("#initials-form");
 var initialsList = document.querySelector("#initials-list");
 var clearScores = document.querySelector("#clear-scores"); 
 
+// parses out the existing highscores or creates an empty array in anticipation of them
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 var latestScore = localStorage.getItem("lastScore");
 finalScore.innerText = latestScore;
 
+// hides the button to clear scores when the page loads
 document.getElementById("clear-scores").hidden = true;
 
+// checks to see if there is an existing score and hides both the instructions to hide the score and the initials input field
 if (latestScore == null) {
   document.getElementById("log-score").hidden = true;
   document.getElementById("initials-form").hidden = true;
 }
 
-
+// maps highscores (array list with key value pairs that combines both initals and score) to individual line items to display on screen
 function renderHighScores() {
   initialsList.innerHTML = highScores.map(score => {
     return `<li class="high-score">${score.initials}-${score.score}</li>`;
@@ -24,11 +27,10 @@ function renderHighScores() {
 };
 
 
-
+// adds high scores to an array of stored scores
 function recordHighScore() {
     
-  // Get stored highScores from localStorage
-  // Parsing the JSON string to an object
+  // Get stored highScores from localStorage and parse the JSON string to the highscores array
   var storedhighScores = JSON.parse(localStorage.getItem("highScores"));
 
   // If highScores were retrieved from localStorage, update the highScores array to it
@@ -37,51 +39,54 @@ function recordHighScore() {
   }
 }
 
-  function storehighScores() {
-    // Stringify and set "highScores" key in localStorage to highScores array
-    localStorage.setItem("highScores", JSON.stringify(highScores));
+function storehighScores() {
+  // Stringify and set "highScores" key in localStorage to highScores array
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+  
+// When the initials are submitted...
+initialsForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  // Return from function early if submitted initialsText is blank
+  if (initialsText === "") {
+    return;
   }
+
+  // then declare an initialsText variable to hold the initials and score in an object
+  var initialsText = {
+    score: latestScore,
+    initials: initialsInput.value.trim()
+  } 
   
-  
-  // When form is submitted...
-  initialsForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    // Return from function early if submitted initialsText is blank
-    if (initialsText === "") {
-      return;
-    }
-
-    // then declare an initialsText variable to hold the initials and score
-    var initialsText = {
-      score: latestScore,
-      initials: initialsInput.value.trim()
-    } 
-    
-    // Add new initials to highScores array, clear the input
-    highScores.push(initialsText);
-    highScores.sort( (a,b) => {
-      return b.score - a.score;
-    });
-
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-
-    initialsInput.value = "";
-    document.getElementById("initials-text").hidden = true;
-    document.getElementById("log-score").hidden = true;
-    document.getElementById("clear-scores").hidden = false;
-
-    renderHighScores();
-
-    // Store updated highScores in localStorage
-    storehighScores();
+  // Add new initials to the highScores array
+  highScores.push(initialsText);
+  highScores.sort( (a,b) => {
+    return b.score - a.score;
   });
 
+  // adds the new array value to local storage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 
-  clearScores.addEventListener("click", function clearHighScores() {
-    console.log("clicked");
-    window.localStorage.clear();
-    document.getElementById("initials-list").hidden = true;
-  });
-  
+  // sets initials input to blank and hides the other informational elements
+  initialsInput.value = "";
+  document.getElementById("initials-text").hidden = true;
+  document.getElementById("log-score").hidden = true;
+  document.getElementById("clear-scores").hidden = false;
+
+  // reprints the highscores list
   renderHighScores();
+
+  // Store updated highScores in localStorage
+  storehighScores();
+});
+
+// clears local storage if the Clear High Scores button is clicked and hides the input form
+clearScores.addEventListener("click", function clearHighScores() {
+  window.localStorage.clear();
+  document.getElementById("initials-list").hidden = true;
+});
+
+// displays any existing high scores to the user upon page load
+renderHighScores();
